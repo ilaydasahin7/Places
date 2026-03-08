de2209e13bc1b866652d7df5cf1a71c4dd89ed71
İlayda Şahin <ilaydasahin@Ilaydas-MacBook-Pro-2.local>
2026-03-08 15:48:47 +0100

    Create Project

---
+++ PlacesTests/PlacesTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlacesTests.swift
+//  PlacesTests
+//
+//  Created by İlayda Şahin on 8.03.2026.
+//
+
+import Testing
+
+struct PlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
